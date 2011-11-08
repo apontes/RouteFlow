605b33ffef937015c2ace82fdff9efd28ab7ff54
Alisson <apontes@cpqd.com.br>
2011-11-08 09:53:34 -0200

    Update rf-controller/src/gui/lib/routeflow/example1.js

--- rf-controller/src/gui/lib/routeflow/example1.js
+++ rf-controller/src/gui/lib/routeflow/example1.js
@@ -190,7 +190,7 @@ function routeFlowDemo(jsonTopology){
 			    }
                         }
 		});
-		setTimeout(statsTimer, 2000);
+		setTimeout(statsTimer, 1000);
 	}
 	
 	function statsTimer() {
@@ -202,7 +202,7 @@ function routeFlowDemo(jsonTopology){
                 	}
 		);
 		rfDemo.printInfo(lastClickedNode);
-		setTimeout(topologyTimer, 2000);
+		setTimeout(topologyTimer, 1000);
 	}
 
 	setTimeout(statsTimer, 2000);
